d693b03dc33222e523d0043309f45d87e8238fbe
Andrew Roberts <aroberts@ci-vs.com>
2022-09-14 16:34:49 -0700

    Add googlePlayActivation

--- Assets/GameCode/PlayerAccount/PlayerAccountInformation.cs
+++ Assets/GameCode/PlayerAccount/PlayerAccountInformation.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
 
 public class PlayerAccountInformation : SingletonClass<PlayerAccountInformation>, IInitializationClass
 {
@@ -25,6 +28,7 @@ public class PlayerAccountInformation : SingletonClass<PlayerAccountInformation>
     public void InitializeClass()
     {
         Debug.Log("PlayerAccountInformation");
+        ActivateGooglePlayServices();
         if (PlayerPrefs.HasKey(TnS_GlobalSettings.TNS_ENEMIESKILLEDLIFETIME))
         {
             EnemiesKilledLifetime = PlayerPrefs.GetInt(TnS_GlobalSettings.TNS_ENEMIESKILLEDLIFETIME);
@@ -34,6 +38,8 @@ public class PlayerAccountInformation : SingletonClass<PlayerAccountInformation>
             EnemiesKilledLifetime = 0;
             PlayerPrefs.SetInt(TnS_GlobalSettings.TNS_ENEMIESKILLEDLIFETIME, EnemiesKilledLifetime);
         }
+
+
         _initialized = true;
     }
 
@@ -41,4 +47,29 @@ public class PlayerAccountInformation : SingletonClass<PlayerAccountInformation>
     {
         
     }
+
+    private void ActivateGooglePlayServices()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        // enables saving game progress.
+        .EnableSavedGames()
+        // requests the email address of the player be available.
+        // Will bring up a prompt for consent.
+        .RequestEmail()
+        // requests a server auth code be generated so it can be passed to an
+        //  associated back end server application and exchanged for an OAuth token.
+        .RequestServerAuthCode(false)
+        // requests an ID token be generated.  This OAuth token can be used to
+        //  identify the player to other services such as Firebase.
+        .RequestIdToken()
+        .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+    }
+
+    
 }
